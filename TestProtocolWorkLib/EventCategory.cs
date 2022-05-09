bfdde8d29f38568fb72e1276c1a8c787a06ee874
Andrey Pisarets <pisarets.andrey@gmail.com>
2022-05-09 20:05:05 +0300

    Добавил класс для категоризации/группировки событий

---
+++ TestProtocolWorkLib/EventCategory.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProtocolWorkLib
+{
+    class EventCategory
+    {
+        [Flags]
+        public enum EventCategories
+        {
+           None = 0,
+           Intruder = 1,
+           SecurityDevices = 2,
+           Operator = 4,
+           Guards = 8
+        }
+
+        public static readonly Dictionary<string, EventCategories> EventsCategoriesDictionary = new()
+        {
+            { "VisionRangeEvent", EventCategories.None },
+            { "CaptureRadiusEvent", EventCategories.None },
+            { "EntranceToZoneEvent", EventCategories.Intruder },
+            { "ExitFromZoneEvent", EventCategories.Intruder },
+            { "ReduceDetSensWeatherEvent", EventCategories.None },
+            { "ReduceDetSensPartnerEvent", EventCategories.None },
+            { "SignResetEvent", EventCategories.None },
+            { "MovToPointEvent", EventCategories.None },
+            { "SpecZoneNotAffVehEvent", EventCategories.None },
+            { "SpecZoneAffVehEvent", EventCategories.None },
+        };
+    }
+}
