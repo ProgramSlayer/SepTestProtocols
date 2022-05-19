82878b3524454e882706b054f46317201461b72b
Andrey Pisarets <pisarets.andrey@gmail.com>
2022-05-19 16:03:17 +0300

    Добавил класс Reader, который извлекает события из файла испытания.

---
+++ TestProtocolWorkLib/Reader.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+
+namespace TestProtocolWorkLib
+{
+    public static class EventsReader
+    {
+        public static XElement ReadTest(string filePath, Encoding encoding)
+        {
+            // TODO: добавить проверки.
+
+            using StreamReader reader = new(filePath, encoding);
+            XElement test = XElement.Load(reader);
+            return test;
+        }
+
+        public static XElement[] GetEvents(XElement test)
+        {
+            // TODO: добавить проверки.
+
+            IEnumerable<XElement> events = test.Elements("Events").Elements();
+            return events.ToArray();
+        }
+    }
+}
---
+++ WpfEventsReader/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml.Linq;
+using TestProtocolWorkLib;
+
+namespace WpfEventsReader
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            //Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+        }
+
+        private class TestEvent
+        {
+            public string Name { get; set; }
+            public string CTime { get; set; }
+        }
+
+        private void btnOpenFile_Click(object sender, RoutedEventArgs e)
+        {
+            using (OpenFileDialog ofd = new() { CheckFileExists = true, CheckPathExists = true, Filter = "XML-файлы (*.xml)|*.xml" })
+            {
+                if (ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    string path = ofd.FileName;
+                    XElement test = EventsReader.ReadTest(path, Encoding.UTF8);
+                    XElement[] events = EventsReader.GetEvents(test);
+                    TestEvent[] source = (from ev in events
+                                          select new TestEvent { Name = ev.Name.LocalName, CTime = ev.Attribute("Ctime").Value })
+                                          .ToArray();
+                    dgEvents.ItemsSource = source;
+                }
+            }
+        }
+    }
+}
