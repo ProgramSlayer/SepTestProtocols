13c034e3a5cc11d1e25874b905142969c784e76c
Andrey Pisarets <pisarets.andrey@gmail.com>
2022-05-27 18:18:15 +0300

    Добавил модель данных события.

---
+++ WpfEventsReader/Models/EventModel.cs
@@ -0,0 +1,32 @@
+﻿using System.Xml.Linq;
+using TestProtocolWorkLib;
+
+namespace WpfEventsReader.Models
+{
+    public class EventModel
+    {
+        public string Name { get; set; }
+        public float Ctime { get; set; }
+
+        public EventCategory.EventCategories Category { get; set; }
+
+        public int Rating { get; set; }
+
+        public int Id { get; set; }
+
+        public EventModel() { }
+
+        // TODO: нужны проверки.
+        public EventModel(XElement ev)
+        {
+            Name = ev.Name.LocalName;
+            _ = float.TryParse(ev.Attribute("Ctime").Value.Replace('.', ','), out float cTime);
+            Ctime = cTime;
+            Category = EventCategory.EventsCategoriesDictionary[Name];
+            _ = int.TryParse(ev.Attribute("EventRating").Value, out int rating);
+            Rating = rating;
+            _ = int.TryParse(ev.Attribute("Id").Value, out int id);
+            Id = id;
+        }
+    }
+}
