43b298b9917311201770b5f96bcb425e3dd0284e
Andrey Pisarets <pisarets.andrey@gmail.com>
2022-07-09 10:54:51 +0300

    Добавлены свойства для хранения индексов CcIndex.

--- WpfEventsReader/Models/TimelineRowModel.cs
+++ WpfEventsReader/Models/TimelineRowModel.cs
@@ -14,6 +14,14 @@ namespace WpfEventsReader.Models
         public string DescriptionCategoryOperator { get; private set; } = "";
         public string DescriptionCategoryGuards { get; private set; } = "";
 
+        public string CategoryNoneCcIndexes { get; set; } = "";
+        public string CategoryIntruderCcIndexes { get; set; } = "";
+        public string CategorySecurityDevicesCcIndexes { get; set; } = "";
+        public string CategoryOperatorCctvCcIndexes { get; set; } = "";
+        public string CategoryOperatorCcIndexes { get; set; } = "";
+        public string CategoryGuardsCcIndexes { get; set; } = "";
+        
+
         public TimelineRowModel(EventModel[] row)
         {
             // TODO: check if events have the same Ctime attribute.
@@ -25,43 +33,94 @@ namespace WpfEventsReader.Models
             StringBuilder operatorCat = new();
             StringBuilder guardsCat = new();
 
+            StringBuilder noneCatCcInd = new();
+            StringBuilder intruderCatCcInd = new();
+            StringBuilder securityDevicesCatCcInd = new();
+            StringBuilder operatorCctvCatCcInd = new();
+            StringBuilder operatorCatCcInd = new();
+            StringBuilder guardsCatCcInd = new();
+
             foreach (EventModel ev in row)
             {
                 switch (ev.Category)
                 {
                     case EventCategory.EventCategories.None:
                         _ = noneCat.AppendLine(ev.Description);
+                        if (ev.CcIndex != 0)
+                        {
+                            _ = noneCatCcInd.Append($"{ev.CcIndex}; ");
+                        }
                         break;
                     case EventCategory.EventCategories.Intruder:
                         _ = intruderCat.AppendLine(ev.Description);
+                        if (ev.CcIndex != 0)
+                        {
+                            _ = intruderCatCcInd.Append($"{ev.CcIndex}; ");
+                        }
                         break;
                     case EventCategory.EventCategories.SecurityDevices:
                         _ = securityDevicesCat.AppendLine(ev.Description);
+                        if (ev.CcIndex != 0)
+                        {
+                            _ = securityDevicesCatCcInd.Append($"{ev.CcIndex}; ");
+                        }
                         break;
                     case EventCategory.EventCategories.Operator:
                         _ = operatorCat.AppendLine(ev.Description);
+                        if (ev.CcIndex != 0)
+                        {
+                            _ = operatorCatCcInd.Append($"{ev.CcIndex}; ");
+                        }
                         break;
                     case EventCategory.EventCategories.OperatorCctv:
                         _ = operatorCctvCat.AppendLine(ev.Description);
+                        if (ev.CcIndex != 0)
+                        {
+                            _ = operatorCctvCatCcInd.Append($"{ev.CcIndex}; ");
+                        }
                         break;
                     case EventCategory.EventCategories.Guards:
                         _ = guardsCat.AppendLine(ev.Description);
+                        if (ev.CcIndex != 0)
+                        {
+                            _ = guardsCatCcInd.Append($"{ev.CcIndex}; ");
+                        }
                         break;
                     case EventCategory.EventCategories.IntruderGuards:
                         _ = intruderCat.AppendLine(ev.Description);
                         _ = guardsCat.AppendLine(ev.Description);
+                        if (ev.CcIndex != 0)
+                        {
+                            _ = intruderCatCcInd.Append($"{ev.CcIndex}; ");
+                            _ = guardsCatCcInd.Append($"{ev.CcIndex}; ");
+                        }
                         break;
                     case EventCategory.EventCategories.SecurityDevicesOperator:
                         _ = securityDevicesCat.AppendLine(ev.Description);
                         _ = operatorCat.AppendLine(ev.Description);
+                        if (ev.CcIndex != 0)
+                        {
+                            _ = securityDevicesCatCcInd.Append($"{ev.CcIndex}; ");
+                            _ = operatorCatCcInd.Append($"{ev.CcIndex}; ");
+                        }
                         break;
                     case EventCategory.EventCategories.OperatorGuards:
                         _ = operatorCat.AppendLine(ev.Description);
                         _ = guardsCat.AppendLine(ev.Description);
+                        if (ev.CcIndex != 0)
+                        {
+                            _ = operatorCatCcInd.Append($"{ev.CcIndex}; ");
+                            _ = guardsCatCcInd.Append($"{ev.CcIndex}; ");
+                        }
                         break;
                     case EventCategory.EventCategories.SecurityDevicesIntruder:
                         _ = securityDevicesCat.AppendLine(ev.Description);
                         _ = intruderCat.AppendLine(ev.Description);
+                        if (ev.CcIndex != 0)
+                        {
+                            _ = securityDevicesCatCcInd.Append($"{ev.CcIndex}; ");
+                            _ = intruderCatCcInd.Append($"{ev.CcIndex}; ");
+                        }
                         break;
                     default:
                         break;
@@ -75,6 +134,13 @@ namespace WpfEventsReader.Models
             DescriptionCategoryOperatorCctv = operatorCctvCat.ToString();
             DescriptionCategoryOperator = operatorCat.ToString();
             DescriptionCategoryGuards = guardsCat.ToString();
+
+            CategoryNoneCcIndexes = noneCatCcInd.ToString();
+            CategoryIntruderCcIndexes = intruderCatCcInd.ToString();
+            CategorySecurityDevicesCcIndexes = securityDevicesCatCcInd.ToString();
+            CategoryOperatorCctvCcIndexes = operatorCctvCatCcInd.ToString();
+            CategoryOperatorCcIndexes = operatorCatCcInd.ToString();
+            CategoryGuardsCcIndexes = guardsCatCcInd.ToString();
         }
     }
 }
